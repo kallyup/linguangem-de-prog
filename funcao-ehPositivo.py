0a5c3b5eaf5ca8aa5a3ca2955e89ddc0218c47aa
Débora Macêdo <deboramacedo886@gmail.com>
2023-05-11 20:09:15 -0300

    maneiras de verifica se é positivo com funcao

---
+++ funcao-ehPositivo.py
@@ -0,0 +1,12 @@
+def ehPositivo(num):
+    if num > 0:
+        print('positivo')
+    else:
+        print('nao e positivo')
+
+
+def ehPositivo(valor):
+    if valor > 0:
+        return True
+    else:
+        return False
