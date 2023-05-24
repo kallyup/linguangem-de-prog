c32d5aee54325303754e1524fc1e77876e7f9e36
Débora Macêdo <deboramacedo886@gmail.com>
2023-05-24 16:29:47 -0300

    fazendo o chat boot de restaurante

--- trabalhoQ1.py
+++ trabalhoQ1.py
@@ -1,20 +1,27 @@
 print("-"*60)
 print('Bem Vidos a Loja da Débora Lídia Barros de Macedo'.center(60))
 print("-"*60)
+# ru -Registro único indetificação
 ru = 4303056
 valor = float(input('digite o valor do produto: '))
 quantidade = int(input('digite a quantidade de produtos desejada: '))
+
 #aplicando descontos
+#Se o cliente comprar entre 10 e 99 unidades ganha 5% de desconto
 if 10 <= quantidade <= 99:
     valordesc = valor - valor * 0.05
     print('Você recebeu 5% de desconto valor atual {}R$ {}{} '.format('\033[1;32m', valordesc*quantidade, '\033[m'))
+#Se o cliente comprar entre 100 e 999 unidades ganha 10% de desconto
 elif 100 <= quantidade <= 999:
     valordesc = valor - valor * 0.10
     print('Você recebeu 10% de desconto valor atual {}R$ {}{} '.format('\033[1;32m', valordesc*quantidade, '\033[m'))
+#Se o cliente comprar apartir de 1000 unidades ganha 15% de desconto
 elif quantidade >= 1000:
     valordesc = valor - valor * 0.15
     print('Você recebeu 15% de desconto valor atual {}R$ {}{} '.format('\033[1;32m', valordesc*quantidade, '\033[m'))
+#Se o cliente comprar menos que 10 unidades não ganha desconto
 else:
     print('Voce não atingiu quantidade suficiente para ganhar desconto. O valor do produto sem desconto é : {}R$ {}{}'.format('\033[1;31m', valor*quantidade, '\033[m'))
+
 print('O valor do produto sem desconto é : {}R$ {}{}'.format('\033[1;31m', valor*quantidade, '\033[m'))
 print('Meu RU é : {}{}{}'.format('\033[1;35;40m', ru, '\033[m'))
---
+++ trabalhoQ2.py
@@ -0,0 +1,63 @@
+print("-"*65)
+print('*Bem Vidos a lanchonete da Débora Lídia Barros de Macedo*'.center(60))
+print("-"*65)
+# ru -Registro único indetificação
+ru = 4303056
+codigo = ('CÓDIGO', 100, 101, 102, 103, 104, 105, 200, 201)
+descricao = ('DESCRIÇÃO', 'Cachorro-Quente', 'Cachorro-Quente Duplo', 'X-Egg', 'X-Salada', 'X-bacon', 'X-tudo', 'Refrigerante em Lata', 'Chá Gelado')
+preco = ('VALOR(R$)', 9.00, 11.00, 12.00, 13.00, 14.00, 17.00, 5.00, 4.00)
+#Mostrando o menu
+print('CARDÁPIO'.center(60))
+print("-"*65)
+for i in range(len(codigo)):
+    print(f'{codigo[i]: <15}', end=' ')
+    print(f'{descricao[i]: ^30}', end=' ')
+    print(f'{preco[i]:>15}')
+
+total = 0
+while True:
+    continuar = input('Deseja continuar? S/N').upper()
+    if continuar == 'S':
+
+        codProduto = int(input('digite o código do produto'))
+        if codProduto == 100:
+            print(f'Você pediu {descricao[1]: ^30} {preco[1]:>10}')
+            total += preco[1]
+            print(f'Total = R${total}')
+        elif codProduto == 101:
+            print(f'Você pediu {descricao[2]: ^30} {preco[2]:>10}')
+            total += preco[2]
+            print(f'Total = R${total}')
+        elif codProduto == 102:
+            print(f'Você pediu {descricao[3]: ^30} {preco[3]:>10}')
+            total += preco[3]
+            print(f'Total = R${total}')
+        elif codProduto == 103:
+            print(f'Você pediu {descricao[4]: ^30} {preco[4]:>10}')
+            total += preco[4]
+            print(f'Total = R${total}')
+        elif codProduto == 104:
+            print(f'Você pediu {descricao[5]: ^30} {preco[5]:>10}')
+            total += preco[5]
+            print(f'Total = R${total}')
+        elif codProduto == 105:
+            print(f'Você pediu {descricao[6]: ^30} {preco[6]:>10}')
+            total += preco[6]
+            print(f'Total = R${total}')
+        elif codProduto == 200:
+            print(f'Você pediu {descricao[7]: ^30} {preco[7]:>10}')
+            total += preco[7]
+            print(f'Total = R${total}')
+        elif codProduto == 201:
+            print(f'Você pediu {descricao[8]: ^30} {preco[8]:>10}')
+            total += preco[8]
+            print(f'Total = R${total}')
+        else:
+            print('opção invalida')
+            continue
+    else:
+        print('O valor total dos produtos é : {}R$ {}{}'.format('\033[1;31m', total, '\033[m'))
+        break
+
+
+print('Meu RU é : {}{}{}'.format('\033[1;35;40m', ru, '\033[m'))
